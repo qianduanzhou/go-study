a0392890496e43484b7c2a963a640b0bfa7c054b
周泓滨 <zhouhongbin@163.com>
2021-09-01 16:06:38 +0800

    feat:更新

--- official/greetings/greetings.go
+++ official/greetings/greetings.go
@@ -21,8 +21,27 @@ func Hello(name string) (string, error) {
 	// message := fmt.Sprintf("Hi, %v. Welcome!", name) // %v是一个占位符，name将会取代他
 
 	message := fmt.Sprintf(randomFormat(), name) // %v是一个占位符，name将会取代他
-
+	// message := fmt.Sprint(randomFormat()) //单元测试TestHelloName不通过的例子
 	return message, nil //nil代表没有错误
+}
+
+// Hellos returns a map that associates each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// In the map, associate the retrieved message with
+		// the name.
+		messages[name] = message
+	}
+	return messages, nil
 }
 
 // init sets initial values for variables used in the function.
--- official/hello/hellow.go
+++ official/hello/hellow.go
@@ -24,8 +24,15 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
+	// A slice of names.
+
 	// Request a greeting message.
-	message, err := greetings.Hello("zhou")
+	// message, err := greetings.Hello("zhou")
+
+	//传入一个[]，返回一个map
+	names := []string{"Gladys", "Samantha", "Darrin"}
+	message, err := greetings.Hellos(names)
+
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
@@ -35,4 +42,5 @@ func main() {
 	// If no error was returned, print the returned message
 	// to the console.
 	fmt.Println(message)
+
 }
