9bfe4ec514fbb3b6653d45d4c25c10204ad2dd43
周泓滨 <zhouhongbin@163.com>
2021-09-08 17:49:49 +0800

    feat: update

--- go基础/基本类型/main.go
+++ go基础/基本类型/main.go
@@ -29,4 +29,39 @@ func main() {
 	//7.join操作（将slice拼接成一个string）
 	slice := []string{"a", "b", "c"}
 	fmt.Println(strings.Join(slice, "，"))
+
+	//byte和rune类型
+	traversalString()
+	changeString()
+
+	//类型转换
+	num := 1
+	fmt.Println("num", float64(num))
+}
+
+// 遍历字符串
+func traversalString() {
+	s := "pprof.cn博客"
+	for i := 0; i < len(s); i++ { //byte
+		fmt.Printf("%v(%c) ", s[i], s[i])
+	}
+	fmt.Println()
+	for _, r := range s { //rune
+		fmt.Printf("%v(%c) ", r, r)
+	}
+	fmt.Println()
+}
+
+//修改字符串
+func changeString() {
+	s1 := "hello"
+	// 强制类型转换
+	byteS1 := []byte(s1)
+	byteS1[0] = 'H'
+	fmt.Println(string(byteS1))
+
+	s2 := "博客"
+	runeS2 := []rune(s2)
+	runeS2[0] = '狗'
+	fmt.Println(string(runeS2))
 }
