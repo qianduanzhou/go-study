7e06e87353e5c7f9ecd1082b8080fba9bd31747a
周泓滨 <zhouhongbin@163.com>
2021-09-26 17:35:54 +0800

    update

--- 数据操作/go操作MySql/main.go
+++ 数据操作/go操作MySql/main.go
@@ -3,10 +3,135 @@ package main
 import (
 	"fmt"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 )
 
+type Person struct {
+	UserId   int    `db:"user_id"`
+	Username string `db:"username"`
+	Sex      string `db:"sex"`
+	Email    string `db:"email"`
+}
+
+type Place struct {
+	Country string `db:"country"`
+	City    string `db:"city"`
+	TelCode int    `db:"telcode"`
+}
+
+var Db *sqlx.DB
+
+func init() {
+	database, err := sqlx.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/go_test")
+	if err != nil {
+		fmt.Println("open mysql failed,", err)
+		return
+	}
+	Db = database
+	// defer Db.Close() // 注意这行代码要写在上面err判断的下面
+}
+
+//数据插入（insert）
+func insert() {
+	r, err := Db.Exec("insert into person(username, sex, email)values(?, ?, ?)", "stu001", "man", "stu01@qq.com")
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		return
+	}
+	id, err := r.LastInsertId()
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		return
+	}
+
+	fmt.Println("insert succ:", id)
+}
+
+//数据查询（select）
+func search() {
+	var person []Person
+	err := Db.Select(&person, "select user_id, username, sex, email from person where user_id=?", 2)
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		return
+	}
+
+	fmt.Println("select succ:", person)
+}
+
+//数据更新（Update）
+func update() {
+	res, err := Db.Exec("update person set username=? where user_id=?", "stu0003", 2)
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		return
+	}
+	row, err := res.RowsAffected()
+	if err != nil {
+		fmt.Println("rows failed, ", err)
+	}
+	fmt.Println("update succ:", row)
+}
+
+//数据删除（Delete）
+func del() {
+	res, err := Db.Exec("delete from person where user_id=?", 2)
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		return
+	}
+
+	row, err := res.RowsAffected()
+	if err != nil {
+		fmt.Println("rows failed, ", err)
+	}
+
+	fmt.Println("delete succ: ", row)
+}
+
+/*mysql事务*/
+func thing() {
+	conn, err := Db.Begin()
+	if err != nil {
+		fmt.Println("begin failed :", err)
+		return
+	}
+
+	r, err := conn.Exec("insert into person(username, sex, email)values(?, ?, ?)", "stu001", "man", "stu01@qq.com")
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		conn.Rollback()
+		return
+	}
+	id, err := r.LastInsertId()
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		conn.Rollback()
+		return
+	}
+	fmt.Println("insert succ:", id)
+
+	r, err = conn.Exec("insert into person(username, sex, email)values(?, ?, ?)", "stu001", "man", "stu01@qq.com")
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		conn.Rollback()
+		return
+	}
+	id, err = r.LastInsertId()
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		conn.Rollback()
+		return
+	}
+	fmt.Println("insert succ:", id)
+
+	conn.Commit()
+}
 func main() {
-	database, err := sqlx.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/test")
-	fmt.Println(database, err)
+	// insert()
+	// search()
+	// update()
+	// del()
+	thing()
 }
